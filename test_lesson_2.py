41c97be9b29d23a66161c6db56593546830681bd
atrun <a.trunilin92@mail.ru>
2023-08-01 18:00:48 +0300

    добавлен новый файл

--- test_lesson_2.py
+++ test_lesson_2.py
@@ -1,7 +1,26 @@
+import pytest
 from selene.support.shared import browser
 from selene import be, have
 
+
+@pytest.fixture()
 def test_selene():
     browser.open('https://google.com')
+
+    yield
+
+
+@pytest.fixture()
+def test_size():
+    browser.set_window_size(800, 600)
+
+
+
+def test_1(test_selene):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_2(test_selene):
+    browser.element('[name="q"]').should(be.blank).type('цвцвцвцвцвцвцвцвцвцвцапапапапапа').press_enter()
+    browser.element('[id="search"]').should(have.text('По запросу ничего не найдено.'))
